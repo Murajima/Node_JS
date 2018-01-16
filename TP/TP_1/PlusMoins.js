e917277c231a50268542ffac132cbe4c772fe30e
Antoine <apoussard@MacBook-Air-de-Poussard.local>
2018-01-16 15:08:54 +0100

    Amélioration plusMoins

--- TP/TP_1/PlusMoins.js
+++ TP/TP_1/PlusMoins.js
@@ -6,18 +6,23 @@ const solution = Math.floor(Math.random() * 10)
 const compteur = 1
 
 function play (compteur) {
-    rl.question('Question ?', (answer) => {
+    rl.question('Entrez un nombre entre 0 et 10 ', (answer) => {
         process.stdout.write("Tu as répondu : " + answer +"\n")
-        if (answer == solution) {
-            console.log('gagné en ' + compteur + " coups")
-        } else if (answer < solution) {
-            console.log('C\'est plus !')
-            compteur += 1
-            play(compteur)
-        } else if (answer > solution) {
-            console.log('C\'est moins')
-            compteur += 1
-            play(compteur)
+        if (/^\d{1,3}$/.test(answer)){
+            if (answer == solution) {
+                console.log('gagné en ' + compteur + " coups")
+            } else if (answer < solution) {
+                console.log('C\'est plus !')
+                compteur += 1
+                play(compteur)
+            } else if (answer > solution) {
+                console.log('C\'est moins')
+                compteur += 1
+                play(compteur)
+            }
+        } else {
+            console.log('Ceci n\'est pas un nombre valide')
+            play()
         }
     })
 }
