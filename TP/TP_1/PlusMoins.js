7619b47cc0ad65491aed6d506c410e99b7fe6fa2
Antoine <apoussard@MacBook-Air-de-Poussard.local>
2018-01-16 15:17:07 +0100

    function restart

--- TP/TP_1/PlusMoins.js
+++ TP/TP_1/PlusMoins.js
@@ -6,11 +6,12 @@ const solution = Math.floor(Math.random() * 10)
 const compteur = 1
 
 function play (compteur) {
-    rl.question('Entrez un nombre entre 0 et 10 ', (answer) => {
+    rl.question('Entrez un nombre entre 0 et 10 \n', (answer) => {
         process.stdout.write("Tu as répondu : " + answer +"\n")
         if (/^\d{1,3}$/.test(answer)){
             if (answer == solution) {
                 console.log('gagné en ' + compteur + " coups")
+                restart()
             } else if (answer < solution) {
                 console.log('C\'est plus !')
                 compteur += 1
@@ -28,5 +29,20 @@ function play (compteur) {
 }
 
 play(compteur)
+
+function restart () {
+    let solution = Math.floor(Math.random() * 10)
+    let compteur = 1
+    rl.question('Voulez vous rejouer? Y/n\n', (answer) => {
+        if(answer == 'Y') {
+            play(compteur)
+        } else if(answer == 'n') {
+            process.exit(1)
+        } else {
+            console.log('Répondez par Y ou n')
+            restart()
+        }
+    })
+}
 
 
