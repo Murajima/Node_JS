16b9641ff48de92742e414fc22a8613fea2af971
Antoine Poussard <antoinepoussard@MacBook-Pro-de-Antoine.local>
2018-04-03 15:37:57 +0200

    controller + router

---
+++ TP/TP_3/controller/todo.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const session = require('express-session')
+const todo = require('../utils/todo.js')
+const user = require('../utils/user.js')
+
+const router = express.Router();
+
+var TODO = {}
+var USER = {}
+var needSync = true
+
+router.all('*', (req, res, next) => {
+    next()
+})
+
+router.post('/todos', (req, res, next) => {
+    var message = req.body.message
+    todo.insertIntoTodo(message).then((result) => {
+        todo.getTodos().then((result) => {
+            needSync = true;
+            res.redirect('/todos')
+        })
+    })
+})
+
+router.get('/todos', (req, res, next) => {
+    var offset = req.param('offset')
+    var limit = req.param('limit')
+    if(offset != null && limit != null ){
+        todo.getTodosOffset(offset, limit).then((result) => {
+
+            res.render('todos/index', {
+                title: 'Bonjour !',
+                name: 'Toto',
+                content: result
+            })
+        })
+    } else {
+        user.getUsers().then((result) => {
+            USER = result
+            todo.getTodos().then((result) => {
+                TODO = result
+                returnDict = []
+                result.forEach(function(todo) {
+                    USER.forEach(function(user){
+                        if(todo.userId == user.id){
+                            todo.name = user.Name
+                        }
+                    })
+                    returnDict.push(todo)
+                })
+                res.render('todos/index', {
+                    title: 'Bonjour !',
+                    name: 'Toto',
+                    content: returnDict
+                })
+            })
+        })
+    }
+})
+
+router.get('/todos/:todoId', (req, res, next) => {
+    var id = req.params.todoId
+    var returnDict = {}
+    TODO.forEach(function(element){
+        if(element.id == id) {
+            returnDict = element
+        }
+    })
+    res.render('todos/show', {
+            title: 'Bonjour !',
+            name: 'Toto',
+            content: returnDict
+    })
+})
+
+router.get('/add', (req, res, next) => {
+    res.render('todos/edit')
+})
+
+router.patch('/todos/:todoId', (req, res, next) => {
+    var id = req.params.todoId
+    todo.patchTodoById(id).then((result) => {
+        res.redirect('/todos')
+    })
+})
+
+router.delete('/todos/:todoId', (req, res, next) => {
+    var id = req.params.todoId
+    todo.deleteTodoById(id).then((result) => {
+        res.redirect('/todos')
+    })
+})
+
+router.use((req, res) => {
+    res.redirect('/todos')
+})
+
+module.exports = router
--- TP/TP_3/server.js
+++ TP/TP_3/server.js
@@ -3,17 +3,9 @@ const session = require('express-session')
 const app = express()
 const PORT = process.env.PORT || 8080
 const bodyParser = require('body-parser')
-const todo = require('./utils/todo.js')
-const user = require('./utils/user.js')
 const methodOverride = require('method-override')
-
-var TODO = {}
-var USER = {}
-var needSync = true
-
-app.all('*', (req, res, next) => {
-    next()
-})
+const todo = require('./controller/todo.js');
+const user = require('./controller/user.js');
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -21,105 +13,9 @@ app.set('views', './views')
 app.set('view engine', 'pug')
 app.use(methodOverride('_method'))
 
-app.post('/todos', (req, res, next) => {
-    var message = req.body.message
-    todo.insertIntoTodo(message).then((result) => {
-        todo.getTodos().then((result) => {
-            needSync = true;
-            res.redirect('/todos')
-        })
-    })
-})
 
-app.post('/users', (req, res, next) => {
-    var username = req.body.username
-    var password = req.body.password
-    user.insertIntoUsers(username, password).then((result) => {
-        user.getUsers().then((result) => {
-            res.send(result)
-        })
-    })
-})
-
-app.get('/todos', (req, res, next) => {
-    var offset = req.param('offset')
-    var limit = req.param('limit')
-    if(offset != null && limit != null ){
-        todo.getTodosOffset(offset, limit).then((result) => {
-
-            res.render('todos/index', {
-                title: 'Bonjour !',
-                name: 'Toto',
-                content: result
-            })
-        })
-    } else {
-        user.getUsers().then((result) => {
-            USER = result
-            todo.getTodos().then((result) => {
-                TODO = result
-                returnDict = []
-                result.forEach(function(todo) {
-                    USER.forEach(function(user){
-                        if(todo.userId == user.id){
-                            todo.name = user.Name
-                        }
-                    })
-                    returnDict.push(todo)
-                })
-                res.render('todos/index', {
-                    title: 'Bonjour !',
-                    name: 'Toto',
-                    content: returnDict
-                })
-            })
-        })
-    }
-})
-
-app.get('/todos/:todoId', (req, res, next) => {
-    var id = req.params.todoId
-    var returnDict = {}
-    TODO.forEach(function(element){
-        if(element.id == id) {
-            returnDict = element
-        }
-    })
-    res.render('todos/show', {
-            title: 'Bonjour !',
-            name: 'Toto',
-            content: returnDict
-    })
-})
-
-app.get('/add', (req, res, next) => {
-    res.render('todos/edit')
-})
-
-app.get('/register', (req, res, next) => {
-    res.render('user/edit')
-})
-
-app.patch('/todos/:todoId', (req, res, next) => {
-    var id = req.params.todoId
-    todo.patchTodoById(id).then((result) => {
-        res.redirect('/todos')
-    })
-})
-
-app.delete('/todos/:todoId', (req, res, next) => {
-    var id = req.params.todoId
-    todo.deleteTodoById(id).then((result) => {
-        res.redirect('/todos')
-    })
-})
-
-
-
-
-app.use((req, res) => {
-    res.redirect('/todos')
-})
+app.use('/users', user)
+app.use('/', todo)
 
 app.listen(PORT, () => {
     console.log('Serveur sur port:', PORT)
